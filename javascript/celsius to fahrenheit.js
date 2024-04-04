f2fb45dbca37b18eb77c5dce876b09efd8418479
Raja-002 <154741441+Raja-002@users.noreply.github.com>
2024-04-04 11:20:49 +0530

    Delete javascript/celsius to fahrenheit.js

--- javascript/celsius to fahrenheit.js
+++
@@ -1,7 +0,0 @@
-function celisus() {
-    let inputval = Number(document.getElementById("input").value);
-    let f = (inputval - 32) * (5 / 9);
-    let result = document.getElementById("result").innerHTML = f.toFixed(3) + " celsius";
-    
-
-}
